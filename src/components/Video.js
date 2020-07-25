b7676e4a9c92e837f82029f5683a366d772b0bcf
Chanho Ahn <moz5691@gmail.com>
2020-07-24 20:40:20 -0400

    initial

---
+++ src/components/Video.js
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react';
+
+/**
+ * Just a component that I use for recording myself during video tutorials.
+ */
+const width = 600;
+const height = 450;
+
+const formatAsPercentage = (x) => `${Math.round(x * 100)}%`;
+
+const Video = ({ yoloData }) => {
+  const videoRef = useRef();
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      });
+  }, []);
+  return (
+    <>
+      <video
+        ref={videoRef}
+        style={{ transform: 'scale(-1, 1)' }}
+        width={width}
+        height={height}
+        id="video"
+        style={{ zIndex: -1 }}
+      />
+      <svg id="canvas" width="600" height="450">
+        {yoloData.map((yolo, i) => (
+          <>
+            <rect
+              x={yolo.normalized.x * width}
+              y={yolo.normalized.y * height}
+              width={yolo.normalized.width * width}
+              height={yolo.normalized.height * height}
+              style={{
+                fill: 'blue',
+                stroke: 'pink',
+                strokeWidth: 3,
+                fillOpacity: 0.1,
+                strokeOpacity: 0.9,
+              }}
+            />
+            <text
+              x={yolo.normalized.x * width + 20}
+              y={(yolo.normalized.y + yolo.normalized.height / 2) * height}
+              fill="white"
+              font-family="Arial, Helvetica, sans-serif"
+              font-size="24"
+            >
+              {yolo.label} -- {formatAsPercentage(yolo.confidence)}
+            </text>
+          </>
+        ))}
+      </svg>
+    </>
+  );
+};
+
+export default Video;
---
+++ src/utils/classification.js
@@ -0,0 +1,20 @@
+import * as ml5 from 'ml5';
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function classifyImg(id) {
+  const image = document.getElementById(id);
+
+  let results = [];
+  try {
+    const classifier = await ml5.imageClassifier('MobileNet');
+    results = await classifier.predict(image);
+    // await delay(2000);
+    console.log('results', results);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+  // await delay(2000);
+  return results;
+}
