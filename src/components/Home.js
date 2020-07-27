ac88e734346918f675ff51ba34170e84c4655114
Chanho Ahn <moz5691@gmail.com>
2020-07-27 12:06:48 -0400

    home page image added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,27 @@ import React from 'react';
 function Home() {
   return (
     <div className="App">
-      <p>Home page</p>
+      <h2>Image Classification</h2>
+      <img
+        src="https://www.rug.nl/research/bernoulli/groups/autonomus-perceptive-systems/plaatjes-pdf/modified_alexnet.png"
+        alt="classification"
+      />
+
+      <h2>YOLO Object Detection</h2>
+      <img
+        src="https://i0.wp.com/analyticsweek.com/wp-content/uploads/2019/11/object_tracker_gif.gif?fit=600%2C338&ssl=1"
+        alt="yolo"
+      />
+
+      <span>
+        <p>Image Sources</p>
+        <a href="https://www.rug.nl/research/bernoulli/groups/autonomus-perceptive-systems/plaatjes-pdf/modified_alexnet.png">
+          www.rug.nl
+        </a>
+        <a href="https://i0.wp.com/analyticsweek.com/wp-content/uploads/2019/11/object_tracker_gif.gif?fit=600%2C338&ssl=1">
+          i0.wp.com
+        </a>
+      </span>
     </div>
   );
 }
