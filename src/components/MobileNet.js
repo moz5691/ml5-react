427acb1431770a8c5daf0df2cd475b7e48fbe7fb
Chanho Ahn <moz5691@gmail.com>
2020-07-26 20:04:08 -0400

    drop image feature to mobilenet

--- src/components/MobileNet.js
+++ src/components/MobileNet.js
@@ -6,6 +6,9 @@ import GridListTile from '@material-ui/core/GridListTile';
 import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { DropzoneDialog } from 'material-ui-dropzone';
 
 import classifyImg from '../utils/classification';
 import formatAsPercentage from '../utils/formatAsPercentage';
@@ -45,6 +48,7 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
+    margin: '10px',
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
@@ -61,6 +65,11 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'left',
     backgroundColor: theme.palette.background.paper,
   },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
 }));
 
 function MobileNet() {
@@ -69,6 +78,8 @@ function MobileNet() {
   const [selected, setSelected] = useState();
   const [predicts, setPredicts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [imageToRender, setImageToRender] = useState();
 
   console.log(imageList);
 
@@ -89,6 +100,7 @@ function MobileNet() {
 
   return (
     <div className="App">
+      <h3>Select or upload an image.</h3>
       <div className={classes.root}>
         <GridList cellHeight={200} className={classes.gridList} cols={4}>
           {imageList.map((image, i) => (
@@ -99,12 +111,18 @@ function MobileNet() {
                 id={image.title}
                 // width="120"
                 // height="100"
-                onClick={() => setSelected(image.title)}
+
+                onClick={() => {
+                  setSelected(image.title);
+                  setLoading(true);
+                  setImageToRender(image.image);
+                }}
               />
             </GridListTile>
           ))}
         </GridList>
-        <h2>Selected image is {selected}</h2>
+        {/* <h2>Selected image is {selected}</h2> */}
+
         <Button
           onClick={() => onSelect(selected)}
           varient="contained"
@@ -118,20 +136,70 @@ function MobileNet() {
         </Button>
       </div>
 
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        {loading ? (
-          <ProgressBar />
-        ) : (
-          <div className={classes.list}>
-            <List>
-              {predicts.map((predict, i) => (
-                <ListItemText key={i}>
-                  {predict.label} {formatAsPercentage(predict.confidence)}
-                </ListItemText>
-              ))}
-            </List>
-          </div>
-        )}
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={() => setOpen(true)}
+        >
+          Add Image
+        </Button>
+
+        <DropzoneDialog
+          acceptedFiles={['image/*']}
+          cancelButtonText={'cancel'}
+          submitButtonText={'submit'}
+          maxFileSize={5000000}
+          open={open}
+          onClose={() => setOpen(false)}
+          onSave={(files) => {
+            console.log('Files:', files);
+            setImageToRender(URL.createObjectURL(files[0]));
+            onSelect('uploaded-image');
+            setOpen(false);
+          }}
+          showPreviews={true}
+          showFileNamesInPreview={true}
+        />
+      </div>
+
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              {imageToRender && (
+                <img
+                  src={imageToRender}
+                  id="uploaded-image"
+                  width="300"
+                  height="300"
+                  alt="uploaded image"
+                />
+              )}
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                {loading ? (
+                  <ProgressBar />
+                ) : (
+                  <div className={classes.list}>
+                    <List>
+                      {predicts.map((predict, i) => (
+                        <ListItemText key={i}>
+                          {predict.label}{' '}
+                          {formatAsPercentage(predict.confidence)}
+                        </ListItemText>
+                      ))}
+                    </List>
+                  </div>
+                )}
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
